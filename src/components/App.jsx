8e47f2d9679d13b54a121e814b6ac0532cbe2c07
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2022-11-18 10:14:07 +0600

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,28 @@
-import { Profile } from "./Profile/Profile";
-import user from "../components/Profile/user.json";
+import { Profile } from './Profile/Profile';
+import user from '../components/Profile/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../components/Statistics/data.json';
 
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101',
+      // }}
     >
       <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -37,6 +37,9 @@ export const Profile = ({ username, tag, location, avatar, stats}) => {
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    stats: PropTypes.number.isRequired,
-
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    })
   }
